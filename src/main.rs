4b12cb9a8d6928e63b2e94e5dd758ee1f7f973ad
it21294198 <it21294198@my.sliit.lk>
2024-10-20 11:12:10 +0530

    swagger added

--- src/main.rs
+++ src/main.rs
@@ -7,52 +7,21 @@ use axum::{
 use axum_swagger_ui::swagger_ui;
 
 async fn hello_world() -> &'static str {
-    "Hello, world test succeed"
-}
-
-fn custom_swagger_ui(openapi_url: &str) -> Html<String> {
-    Html(format!(
-        r#"
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>SwaggerUI</title>
-    <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@4.5.0/swagger-ui.css" />
-</head>
-<body>
-    <div id="swagger-ui"></div>
-    <script src="https://unpkg.com/swagger-ui-dist@4.5.0/swagger-ui-bundle.js" crossorigin></script>
-    <script>
-        window.onload = () => {{
-            window.ui = SwaggerUIBundle({{
-                url: '{}',
-                dom_id: '#swagger-ui',
-                presets: [
-                    SwaggerUIBundle.presets.apis,
-                    SwaggerUIBundle.SwaggerUIStandalonePreset
-                ],
-                layout: "BaseLayout",
-            }});
-        }};
-    </script>
-</body>
-</html>
-        "#,
-        openapi_url
-    ))
+    "Hello, world test succeeded"
 }
 
 async fn swagger_ui_handler() -> impl IntoResponse {
-    custom_swagger_ui("/swagger/openapi.json")
+    // Provide the URL for the OpenAPI specification
+    let spec_url = "/swagger/openapi.json";
+    // Call the swagger_ui function with the spec_url
+    Html(swagger_ui(spec_url))  // Wrap the response in Html
 }
 
 async fn openapi_json_handler() -> impl IntoResponse {
     (
         StatusCode::OK,
         [("Content-Type", "application/json")],
-        include_str!("openapi.json"),
+        include_str!("openapi.json"), // Ensure this file is correctly located
     )
 }
 
@@ -60,9 +29,9 @@ async fn openapi_json_handler() -> impl IntoResponse {
 async fn main() -> shuttle_axum::ShuttleAxum {
     let doc_url = "/swagger/openapi.json";
     let app = Router::new()
-        .route("/swagger", get(swagger_ui_handler))
-        .route(doc_url, get(openapi_json_handler))
-        .route("/", get(hello_world));
+        .route("/swagger", get(swagger_ui_handler)) // Serve the Swagger UI at this route
+        .route(doc_url, get(openapi_json_handler))  // Serve the OpenAPI JSON spec
+        .route("/", get(hello_world));               // Your other route
 
     Ok(app.into())
 }
